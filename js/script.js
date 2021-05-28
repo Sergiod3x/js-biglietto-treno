e51cf5bcf89dab38a4d1b5f7aa6425fb6ed93dc2
Sergiod3x <sergiod3x.@gmail.com>
2021-05-28 16:41:07 +0200

    standard version completed

--- js/script.js
+++ js/script.js
@@ -4,7 +4,17 @@ var km = prompt("Inserisci il numero dei km");
 var age = prompt("inserisci il numero dei tuoi anni");
 var priceEKm = 0.21;
 var totalPrice = priceEKm * km;
-var priceRound =Math.round (totalPrice * 100) / 100
+
+if(age>65){
+    var totalPriceDis = totalPrice * 0.6; //40% di sconto
+}else{
+    if(age<18){
+        var totalPriceDis = totalPrice * 0.8; //20% di sconto
+    } else{
+        var totalPriceDis = totalPrice;
+    }
+}
+var priceRound =Math.round (totalPriceDis * 100) / 100;
 
 
 
